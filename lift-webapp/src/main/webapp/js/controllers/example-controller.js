cb086d4e3acb6d9533625c5a390af421b1de039e
Cyrus Team <cyrusteam@cyruslists.com>
2013-08-27 16:03:50 -0400

    Getting setup for server communication to grab a selection of things from lift.

---
+++ lift-webapp/src/main/webapp/js/controllers/example-controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+Skeleton.Controllers.controller('ExampleController', function ($scope) {
+    $scope.something = "SOMETHING";
+    $scope.things = [{name: 'John Doe', type: 'person', age: 23}, {name: 'Chrystler', type: 'make', age: 100}];
+});
